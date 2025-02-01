87ace9906c64df9fbfcb8355201970366aab3426
Suresh <suresh@datanetiix.com>
2025-02-01 11:35:21 +0530

    inital commit

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,13 +1,66 @@
-/**
- * @format
- */
+// App.test.js or App.test.tsx
 
 import React from 'react';
-import ReactTestRenderer from 'react-test-renderer';
 import App from '../App';
+import { fireEvent, render } from '@testing-library/react-native';
 
-test('renders correctly', async () => {
-  await ReactTestRenderer.act(() => {
-    ReactTestRenderer.create(<App />);
+describe('App', () => {
+  it('renders LoginScreen initially', () => {
+    const { getByPlaceholderText, getByText } = render(<App />);
+
+    // Ensure login elements are present
+    expect(getByPlaceholderText('Enter username')).toBeTruthy();
+    expect(getByPlaceholderText('Enter Password')).toBeTruthy();
+    expect(getByText('LOGIN')).toBeTruthy();
+  });
+
+  it('displays an error message for empty login', () => {
+    const { getByText } = render(<App />);
+
+    // Trigger login without entering values
+    fireEvent.press(getByText('LOGIN'));
+
+    // Ensure error message is displayed
+    expect(getByText('Please provide all values')).toBeTruthy();
+  });
+
+  it('displays a success message for correct login', () => {
+    const { getByPlaceholderText, getByText } = render(<App />);
+
+    // Enter correct username and password
+    fireEvent.changeText(
+      getByPlaceholderText('Enter username'),
+      'codeb@gmail.com',
+    );
+    fireEvent.changeText(
+      getByPlaceholderText('Enter Password'),
+      'Password@1234',
+    );
+
+    // Trigger login
+    fireEvent.press(getByText('LOGIN'));
+
+    // Ensure success message is displayed
+    expect(getByText('CODEB')).toBeTruthy();
+  });
+
+  it('displays an error message for incorrect login', () => {
+    const { getByPlaceholderText, getByText } = render(<App />);
+
+    // Enter incorrect username and password
+    fireEvent.changeText(
+      getByPlaceholderText('Enter username'),
+      'incorrect@gmail.com',
+    );
+    fireEvent.changeText(
+      getByPlaceholderText('Enter Password'),
+      'IncorrectPassword',
+    );
+
+    // Trigger login
+    fireEvent.press(getByText('LOGIN'));
+
+    // Ensure error message is displayed
+    expect(getByText('INCORRECT CREDENTIAL')).toBeTruthy();
   });
 });
